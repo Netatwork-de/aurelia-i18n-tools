41e53902c88ceca17202ff272d501c8fee48444b
Max J. Polster <max.polster@id-p.de>
2020-03-21 17:11:47 +0100

    Add parse5 utilities

---
+++ src/types/parse5-default-tree-adapter.ts
@@ -0,0 +1,44 @@
+
+declare module "parse5/lib/tree-adapters/default" {
+	import * as lib from "parse5";
+
+	// Typedefs for DefaultTreeDocumentFragment are wrong:
+	interface Template {
+	}
+
+	interface DefaultTreeAdapter extends lib.TreeAdapter {
+		createDocument(): lib.DefaultTreeDocument;
+		createDocumentFragment(): lib.DefaultTreeDocumentFragment;
+		createElement(tagName: string, namespaceURI: string, attrs: lib.Attribute[]): lib.DefaultTreeElement;
+		createCommentNode(data: string): lib.DefaultTreeCommentNode;
+		appendChild(parentNode: lib.DefaultTreeParentNode, newNode: lib.DefaultTreeNode): void;
+		insertBefore(parentNode: lib.DefaultTreeParentNode, newNode: lib.DefaultTreeNode, referenceNode: lib.DefaultTreeNode): void;
+		setTemplateContent(templateElement: lib.DefaultTreeElement, contentElement: lib.DefaultTreeDocumentFragment): void;
+		getTemplateContent(templateElement: lib.DefaultTreeElement): lib.DefaultTreeDocumentFragment;
+		setDocumentType(document: lib.DefaultTreeDocument, name: string, publicId: string, systemId: string): void;
+		setDocumentMode(document: lib.DefaultTreeDocument, mode: lib.DocumentMode): void;
+		getDocumentMode(document: lib.DefaultTreeDocument): lib.DocumentMode;
+		detachNode(node: lib.DefaultTreeNode): void;
+		insertText(parentNode: lib.DefaultTreeParentNode, text: string): void;
+		insertTextBefore(parentNode: lib.DefaultTreeParentNode, text: string, referenceNode: lib.DefaultTreeNode): void;
+		adoptAttributes(recipient: lib.DefaultTreeElement, attrs: lib.Attribute[]): void;
+		getFirstChild(node: lib.DefaultTreeParentNode): lib.DefaultTreeNode;
+		getChildNodes(node: lib.DefaultTreeParentNode): lib.DefaultTreeNode[];
+		getParentNode(node: lib.DefaultTreeChildNode): lib.DefaultTreeParentNode;
+		getAttrList(element: lib.DefaultTreeElement): lib.Attribute[];
+		getTagName(element: lib.DefaultTreeElement): string;
+		getNamespaceURI(element: lib.DefaultTreeElement): string;
+		getTextNodeContent(textNode: lib.DefaultTreeTextNode): string;
+		getCommentNodeContent(commentNode: lib.DefaultTreeCommentNode): string;
+		getDocumentTypeNodeName(doctypeNode: lib.DocumentType): string;
+		getDocumentTypeNodePublicId(doctypeNode: lib.DocumentType): string;
+		getDocumentTypeNodeSystemId(doctypeNode: lib.DocumentType): string;
+		isTextNode(node: lib.DefaultTreeNode): node is lib.DefaultTreeTextNode;
+		isCommentNode(node: lib.DefaultTreeNode): node is lib.DefaultTreeCommentNode;
+		isDocumentTypeNode(node: lib.DefaultTreeNode): node is lib.DefaultTreeDocumentType;
+		isElementNode(node: lib.DefaultTreeNode): node is lib.DefaultTreeElement;
+	}
+
+	const adapter: DefaultTreeAdapter;
+	export = adapter;
+}
---
+++ src/utility/parse5-tree.ts
@@ -0,0 +1,56 @@
+import { DefaultTreeNode, DefaultTreeElement, DefaultTreeParentNode } from "parse5";
+import * as adapter from "parse5/lib/tree-adapters/default";
+
+const IGNORED_NODE_NAMES = new Set(["#comment", "#documentType", "#text"]);
+
+export function * traverseElements(node: DefaultTreeNode, ignoreTagNames: (tagName: string) => boolean): Generator<DefaultTreeElement> {
+	if (IGNORED_NODE_NAMES.has(node.nodeName)) {
+		return;
+	}
+	if (adapter.isElementNode(node)) {
+		if (ignoreTagNames(node.tagName)) {
+			return;
+		}
+		yield node;
+		if (node.nodeName === "template") {
+			yield * traverseElements(adapter.getTemplateContent(node), ignoreTagNames);
+		}
+	}
+	if (isParentNode(node)) {
+		for (const childNode of node.childNodes) {
+			yield * traverseElements(childNode, ignoreTagNames);
+		}
+	}
+}
+
+export function isParentNode(node: DefaultTreeNode): node is DefaultTreeParentNode {
+	return Array.isArray((node as DefaultTreeParentNode).childNodes);
+}
+
+export function getAttributeLocationInfo(element: DefaultTreeElement, name: string) {
+	return element.sourceCodeLocation!.attrs[name];
+}
+
+export function getAttributeValue(element: DefaultTreeElement, name: string): string | undefined {
+	for (const attribute of element.attrs) {
+		if (attribute.name === name) {
+			return attribute.value;
+		}
+	}
+}
+
+export function analyzeElementContent(element: DefaultTreeElement) {
+	let hasText = false;
+	let hasElements = false;
+	for (const node of element.childNodes) {
+		if (adapter.isTextNode(node)) {
+			hasText = true;
+		} else if (adapter.isElementNode(node)) {
+			hasElements = true;
+		}
+		if (hasText && hasElements) {
+			break;
+		}
+	}
+	return { hasText, hasElements };
+}

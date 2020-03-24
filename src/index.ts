3405ef5036193caaf4002d2f88b9d9f32305f624
Max J. Polster <max.polster@id-p.de>
2020-03-24 12:39:23 +0100

    Add index module.

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from "./aurelia-i18n-attribute";
+export * from "./aurelia-template-file";
+export * from "./config";
+export * from "./json-resource-file";
+export * from "./locale-data";
+export * from "./source";
+export * from "./translation-data";
